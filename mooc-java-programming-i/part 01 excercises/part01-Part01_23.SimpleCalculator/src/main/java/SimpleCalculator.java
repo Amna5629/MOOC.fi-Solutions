7a55029cb89f0516f657eda28ba482342499294a
Amna5629 <amnayoosuf123@gmail.com>
2025-04-30 13:07:59 +0530

    Update SimpleCalculator.java

--- mooc-java-programming-i/part 01 excercises/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ mooc-java-programming-i/part 01 excercises/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -6,7 +6,19 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+         System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        //expected double answer for division
+        double answer=(double)first/second;
+
+        System.out.println(first +" + "+ second + " = " + (first+second));
+        System.out.println(first +" - "+ second + " = " + (first-second));
+        System.out.println(first +" * "+ second + " = " + (first*second));
+        System.out.println(first +" / "+ second + " = " + answer);
+
 
     }
 }
